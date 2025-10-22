f9a85fda9f5e3eaf5682fe772ef97ca6d28289e3
anamontero14 <ana.montero@iesnervion.es>
2025-10-22 10:20:46 +0200

    Ejercicio equipos terminado la parte 1 pero no arranca

--- TEMA1/api_equipo/equipo.py
+++ TEMA1/api_equipo/equipo.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -15,12 +15,75 @@ equipo_list = [Equipo(id=1 ,nombre="Equipo1", ciudad="Sevilla", año_fundacion="
               Equipo(id=2 ,nombre="Equipo2", ciudad="Madrid", año_fundacion="1934", estadio="Estadio23"),
               Equipo(id=3 ,nombre="Equipo3", ciudad="Málaga", año_fundacion="2000", estadio="Estadio55")]
 
+
+#devuelve todos los equipos
 @app.get("/equipos")
 def equipos():
     return equipo_list
 
 @app.get("/equipos/{id_equipo}")
-def get_user(id_equipo: int):
-    equipos = [user for user in equipo_list if user.id == id_equipo]
+def get_equipo(id_equipo: int):
+    equipos = [equipo for equipo in equipo_list if equipo.id == id_equipo]
 
     return equipos[0] if equipos else {"Error" : "User not found"}
+
+#método post para añadirlo a la lista
+@app.post("/equipos", status_code=201, response_model=Equipo)
+#recibe un equipo de tipo equipo para meterlo a la lista
+def add_equipo(equipo: Equipo):
+    #al equipo pasado por parámetro se le asigna el siguiente id
+    equipo.id = siguiente_id()
+    #finalmente se añade a la lista
+    equipo_list.append(equipo)
+    #devuelve el mismo equipo
+    return equipo
+
+#método put para actualizar un usuario concreto
+@app.put("/equipos/{id}", response_model=Equipo)
+#se le pasará el id que tiene el equipo y sus datos a actualizar
+def modificar_equipo(id: int, equipo: Equipo):
+    #for para que devuelva el índice y el usuario
+    for index, equipo_guardado in enumerate(equipo_list):
+        #si el equipo que hay en la lista tiene el mismo
+        #id que el que se le pasa por parámetros
+        if equipo_guardado.id == id:
+            #machaco el id que tenga el objeto equipo y le asigno el
+            #que se le pasa por parámetro
+            equipo.id = id
+            #en la lista actualizo al equipo
+            equipo_list[index] = equipo
+            #se retorna el equipo modificado
+            return equipo
+        
+    #si no se encuentra lanza una excepción
+    raise HTTPException(status_code=404, detail="Equipo no encontrado")
+
+#método delete para poder eliminar a un equipo de la lista
+@app.delete("/equipos/{id}")
+#se le pasa el id del equipo a eliminar
+def eliminar_equipo(id: int):
+    #se recorre la lista de los equipos
+    for equipo_guardado in equipo_list:
+        #si el id del equipo actual es igual al id pasado por parámetro
+        if equipo_guardado.id == id:
+            #se elimina al equipo de la lista
+            equipo_list.remove(equipo_guardado)
+            #se devuelve el diccionario vacío
+            return {}
+    
+    #si no se encuentra el id del equipo a eliminar lanza una excepcion
+    raise HTTPException(status_code=404, detail="Equipo no encontrado")
+
+
+#método para buscar equipos
+def buscar_equipos(id: int):
+    equipos = [equipo for equipo in equipo_list if equipo.id == id]
+
+    if equipos:
+        return equipos[0]
+    raise HTTPException(status_code=404, detail="Equipo no encontrado")
+
+#método para indicar el siguiente id
+def siguiente_id():
+    #devuelve el usuario con la id máxima y a este le suma 1 a su id
+    return (max(equipo_list, key=id).id+1)
--- TEMA1/api_users/users.py
+++ TEMA1/api_users/users.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 #entidad user
 class User(BaseModel):
     id: int
@@ -34,10 +35,50 @@ def search_user(id: int):
         return users[0]
     raise HTTPException(status_code=404, detail="User not found")
 
+def next_id():
+    #devuelve el usuario con la id máxima y después le suma 1 para pasar al siguiente id
+    #para la persona a añadir
+    return (max(users_list, key=id).id+1)
+
 @app.get("/users/{id}")
 def get_user(id: int):
     return search_user(id)
 
-@app.get("/users/")
-def get_user(id: int):
-    return search_user(id)
+@app.post("/users", status_code=201, response_model=User)
+#recibe un usuario de tipo usuario para meter a la lista
+def add_user(user : User):
+    #al usuario introducido se le asigna el id correcto
+    user.id = next_id()
+    #se añade el usuario a la lista
+    users_list.append(user)
+    #se devuelve el usuario atualizado
+    return user
+
+@app.put("/users/{id}", response_model=User)
+def modify_user(id: int, user: User):
+    #for para que devuelva el índice y el usuario
+    for index, saved_user in enumerate(users_list):
+        #si el usuario que yo tengo en la lista es igual
+        #al id que recibo
+        if saved_user.id == id:
+            #machaco el id del usuario por el que se pasa por parámetros
+            user.id = id
+            #en la lista de users actualizo el user
+            users_list[index] = user
+            #y se retorna
+            return user
+    
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.delete("/users/{id}")
+def delete_user(id:int):
+    #recorre la lista
+    for saved_user in users_list:
+        #si los ids de la lista de usuarios y el que se pasa coinciden
+        if saved_user.id == id:
+            #se elimina de la lista el usuario
+            users_list.remove(saved_user)
+            #se devuelve el diccionario vacío
+            return {}
+    #si no se encuentra devuelve una excepcion
+    raise HTTPException(status_code=404, detail="User not found")
